ca879977a4357f42d18e7eeb1b8373cf58df27d2
Kolad2 <koladik@gmail.com>
2025-02-12 06:17:15 +0300

    subadd 2

--- extractor.py
+++ extractor.py
@@ -2,18 +2,46 @@ import numpy as np
 
 
 class Extractor:
-	def __init__(self, marked_ares):
-		self.marked_ares = marked_ares
+    def __init__(self, marked_ares):
+        self.marked_ares = marked_ares
 
-	def extruct_centers(self):
-		U, FU, CU = np.unique(self.marked_ares, return_index=True, return_counts=True)
+    def extruct_centers(self, indent=0):
+        array_flat = self.marked_ares.flatten()
+        array_flat_sorted_indices = array_flat.argsort()
+        array_sorted_flat = array_flat[array_flat_sorted_indices]
+        (unique,
+         first,
+         count) = np.unique(
+            array_sorted_flat,
+            return_index=True,
+            return_counts=True
+        )
 
-		return 0
+        y, x = np.indices(self.marked_ares.shape)
+        x = x.flatten()[array_flat_sorted_indices]
+        y = y.flatten()[array_flat_sorted_indices]
 
-	def extruct_areas(self):
-		unique, counts = np.unique(self.marked_ares, return_counts=True)
-		return counts[2:-1]
+        test = [_x for _x in x if _x < 0]
 
-	def extruct(self):
-		return self.extruct_areas()
+        centers = np.zeros((len(unique)-indent, 2))
+
+        for i in range(indent, len(unique)):
+            x_shape = x[first[i]:first[i] + count[i]]
+            y_shape = y[first[i]:first[i] + count[i]]
+            x_center = np.sum(x_shape/len(x_shape))
+            y_center = np.sum(y_shape/len(y_shape))
+            if x_center < 0:
+                print(i,x_shape, np.sum(x_shape))
+                print(x_center)
+            center = np.array((x_center, y_center))
+            centers[i-indent] = center
+
+        return unique[indent:], centers
+
+    def extruct_areas(self):
+        unique, counts = np.unique(self.marked_ares, return_counts=True)
+        return counts[2:-1]
+
+    def extruct(self):
+        return self.extruct_areas()
 

f9c6262e58f3eb238e9e8ce60fa95f4ab0576aeb
Kolad2 <koladik@gmail.com>
2025-02-12 05:32:59 +0300

    subadd 1

--- extractor.py
+++ extractor.py
@@ -5,6 +5,11 @@ class Extractor:
 	def __init__(self, marked_ares):
 		self.marked_ares = marked_ares
 
+	def extruct_centers(self):
+		U, FU, CU = np.unique(self.marked_ares, return_index=True, return_counts=True)
+
+		return 0
+
 	def extruct_areas(self):
 		unique, counts = np.unique(self.marked_ares, return_counts=True)
 		return counts[2:-1]
